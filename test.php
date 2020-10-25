1201691b3ccc379cdcb2c1787b1d7e577d4e26f3
ctibord <73441689+ctibord@users.noreply.github.com>
2020-10-25 20:14:32 +0100

    Test

---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+require_once("index.php");
+$ct = new CtiborD();
+echo("Scitani 1 + 1: ".$ct->intcalc(1, 1, true, false, false, false));
+echo("Odcitani 1 - 1: ".$ct->intcalc(1, 1, false, true, false, false));
+echo("Nasobeni 1 * 1: ".$ct->intcalc(1, 1, false, false, true, false));
+echo("Deleni 1 / 1: ".$ct->intcalc(1, 1, false, false, false, true));
+echo("Chyba deleni: ".$ct->intcalc(1, 0, false, false, false, true));
