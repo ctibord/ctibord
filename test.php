23375fd171b88d8c4a95a1996f5f790e550a050f
ctibord <73441689+ctibord@users.noreply.github.com>
2021-10-02 14:52:18 +0200

    Add modulo to tests

--- test.php
+++ test.php
@@ -5,4 +5,14 @@ echo("Scitani 1 + 1: ".$ct->intcalc(1, 1, true, false, false, false));
 echo("Odcitani 1 - 1: ".$ct->intcalc(1, 1, false, true, false, false));
 echo("Nasobeni 1 * 1: ".$ct->intcalc(1, 1, false, false, true, false));
 echo("Deleni 1 / 1: ".$ct->intcalc(1, 1, false, false, false, true));
-echo("Chyba deleni: ".$ct->intcalc(1, 0, false, false, false, true));
+try {
+    echo("Chyba deleni: ".$ct->intcalc(1, 0, false, false, false, true));
+} catch (Error $e) {
+    echo("Chyba deleni zachycena! ".$e->getMessage());
+}
+echo("Modulo 3 % 2: ".$ct->intcalc(3, 2, false, false, false, false, true));
+try {
+    echo("Chyba modulo: ".$ct->intcalc(1, 0, false, false, false, false, true));
+} catch (Error $e) {
+    echo("Chyba modulo zachycena! ".$e->getMessage());
+}
